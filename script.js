76467e4942f208297215ff7151abecb8a9eb44ff
lbouweriks <88744104+lbouweriks@users.noreply.github.com>
2021-08-15 16:32:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("mybutton").onclick = function () {
+  alert("button clicked");
+};
+
+document.getElementById("bgbutton").onclick = function () {
+  document.body.classList.toggle("red-background");
+};
